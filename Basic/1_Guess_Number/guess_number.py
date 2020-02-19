58ff02c94d91b3952cf4a2c1e9e0b8af96b3c4f4
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2020-02-19 18:37:06 +0100

    Fixed some of PEP8 violation - used pep8 from terminal to do so

--- Basic/1_Guess_Number/guess_number.py
+++ Basic/1_Guess_Number/guess_number.py
@@ -1,34 +1,34 @@
-# This is a program which randomly chooses a number to guess and then 
-# the user will have a few chances to guess the number correctly. 
+# This is a program which randomly chooses a number to guess and then
+# the user will have a few chances to guess the number correctly.
 # In each wrong attempt, the computer will give a hint that the number
 # is greater or smaller than the one you have guessed.
-# The user can choose the difficult of the game: this choise 
-# change the range of the number randomly generated and the 
-# number of attempts. 
+# The user can choose the difficult of the game: this choise
+# change the range of the number randomly generated and the
+# number of attempts.
 
 import random
 
 
-class SkillException(Exception): 
+class SkillException(Exception):
     # Exception for invalid parameter
     def __init__(self, value):
         self.value = value
-    
+
     def __str__(self):
         return "{} is an invalid parameter: ".format(self.value) + \
                 "are only accepted 'S' or 'C'!"
 
 
 def number_generation(skill):
-    # Generate a number according to the skill selected by the user. 
+    # Generate a number according to the skill selected by the user.
     if skill.upper() == 'S':
         min, max = 0, 10  # Range in the simple case
-        attempts = 4 
+        attempts = 4
     elif skill.upper() == 'C':
         min, max = 0, 50  # Range in the complex case
         attempts = 8
     else:
-        raise SkillException(skill.upper()) 
+        raise SkillException(skill.upper())
     num_guess = random.randint(min, max)
     return num_guess, attempts
 
@@ -45,10 +45,10 @@ def compare_numbers(value1, value2):
 
 if __name__ == "__main__":
     DEBUG = 0  # For debug print
-    NUMBERS_ARE_EQUAL, NUMBER_GREATER, NUMBER_LOWER = 0, 1, 2 
+    NUMBERS_ARE_EQUAL, NUMBER_GREATER, NUMBER_LOWER = 0, 1, 2
 
     skill = input("Do you want a simple or a complex game? [s, c]\n=> ")
-    num_guess, attempts = number_generation(skill) 
+    num_guess, attempts = number_generation(skill)
 
     if DEBUG:  # If debug = 1 prints the number to guess and the attempts
         print(num_guess, attempts)
@@ -58,24 +58,24 @@ if __name__ == "__main__":
         # and check if it is the number generated or not, with a print of
         # an hint to help the user.
         try:
-            value = int(input("\nInsert your {0:d}° value: ".format(attempt + 1)))
+            value = int(input("\nInsert your {0:d}°".format(attempt + 1) +
+                              " value: "))
         except:
             raise TypeError("Only numbers are allowed!")
+
         retValue = compare_numbers(value, num_guess)
         if retValue == NUMBERS_ARE_EQUAL:
-            print("You won! The number generated " \
-                    "was {0:d}!".format(num_guess))
+            print("You won! The number generated "
+                  "was {0:d}!".format(num_guess))
             break
         elif retValue == NUMBER_GREATER:
-            print("=> Nope, your value is greater than the " \
-                    "number to guess!", end = ' ')
+            print("=> Nope, your value is greater than the "
+                  "number to guess!", end=' ')
         elif retValue == NUMBER_LOWER:
-            print("=> Nope, your value is lower than the " \
-                    "number to guess!", end = ' ')
+            print("=> Nope, your value is lower than the "
+                  "number to guess!", end=' ')
 
         if attempt == attempts - 1:  # If it is the last attempt print
             print("Game over!")      # the "Game over!" message.
         else:
             print("Try again!")
-            
-    
