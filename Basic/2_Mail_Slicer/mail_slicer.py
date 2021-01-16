ad28d15e5f8d00e5bfcfd15b7aed5d5c16fe040a
AlessioPani <60981463+AlessioPani@users.noreply.github.com>
2021-01-16 16:57:59 +0100

    Improved exception handling and slicing function

--- Basic/2_Mail_Slicer/mail_slicer.py
+++ Basic/2_Mail_Slicer/mail_slicer.py
@@ -6,11 +6,11 @@
 import argparse  # Argument parser
 
 
-def slice(email):
+def slice(email, position_at):
     # This function will print the domain and the username of an email
-    username = email[:email.find('@')].lower()
-    domain = email[email.find('@') + 1:].lower()
-    print('Username: {} | Domain: {}'.format(username, domain))
+    username = email[:position_at].lower()
+    domain = email[position_at + 1:].lower()
+    print('\nUsername: {} | Domain: {}'.format(username, domain))
 
 
 def wrapper(email):
@@ -18,14 +18,16 @@ def wrapper(email):
     # - If not, raise an exception with a message for the user;
     # - If yes, call the slice function.
     position_at = email.find('@')
-    position_dot = email.find('.')
 
-    if email.count('@') == 0 or email.count('@') > 1:
-        raise ValueError('Please, insert a valid email!')
-    elif email[position_at + 1:].count('.') == 0:
-        raise ValueError('Please, insert a valid email!')
-    else:
-        slice(email)
+    try:
+        if email.count('@') == 0 or email.count('@') > 1:
+            raise ValueError
+        elif email[position_at + 1:].count('.') == 0:
+            raise ValueError
+        else:
+            slice(email, position_at)
+    except ValueError as err:
+        print('\nPlease, insert a valid email!')
 
 
 if __name__ == "__main__":
@@ -40,7 +42,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Check if the user didn't provide the appropriate argument
-    if args.mail is None:
-        raise AttributeError('Please provide an email! Use -h for help.')
-    else:
-        wrapper(args.mail)
+    try:
+        if args.mail is None:
+            raise AttributeError
+        else:
+            wrapper(args.mail)
+    except AttributeError:
+        print('\nPlease provide an email! Use -h for help.')
